38708c8e5e815429e268582e0459a5d75fa476de
Lean Gomez <gomezleandro149@gmail.com>
2023-05-02 01:14:01 -0300

    Final commit antes de la entrega del parcial

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
-window.addEventListener("scroll", function(){
-    var nav = document.querySelector("nav");
-    nav.classList.toggle("abajo", window.scrollY>0);
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
 })
