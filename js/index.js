34229100b37eb705098a893396862f84fd69d908
MatiBatt <battitimatias@gmail.com>
2023-06-22 10:41:52 -0300

    Form funcional con JS a un Mail

--- js/index.js
+++ js/index.js
@@ -21,3 +21,35 @@ setTimeout(function(){
 window.addEventListener("load", function(){
     this.document.getElementById("loader").classList.toggle("loader2")
 },)*/
+
+
+// Form with Submit responsive 
+
+document.getElementById("miFormulario").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita el envío del formulario vacio
+  
+    var formulario = document.getElementById("miFormulario");
+  
+    if (formulario.checkValidity()) {
+      // Chequea que los campos esten completados
+      var nombre = document.getElementById("formGroupExampleInput").value;
+      var email = document.getElementById("exampleInputEmail1").value;
+      var mensaje = document.getElementById("exampleFormControlTextarea1").value;
+  
+      var templateParams = {
+        from_name: nombre,
+        from_email: email,
+        message: mensaje
+      };
+  
+      emailjs.send('desarrolloweb', 'Desarrollo_web', templateParams, '21Rut5Jru_CH4uJDF')
+        .then(function(response) {
+          alert('¡El correo se ha enviado correctamente!');
+          
+          // Vacia los campos del formulario una vez que se envio
+          formulario.reset();
+        })
+    } 
+  });
+  
+  
